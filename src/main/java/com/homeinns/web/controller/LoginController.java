20445815fa1fd4ff4879d6e7cc68e0d78eb9c03b
haoxiaoyong1014 <hxyHelloWorld@163.com>
2018-03-10 14:17:03 +0800

    hxy

---
+++ src/main/java/com/homeinns/web/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.homeinns.web.controller;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.common.OAuthProviderType;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Irving on 2014/7/6.
+ */
+@Controller
+@RequestMapping("/test")
+public class LoginController {
+
+    private static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping(value = "/signin" ,method = RequestMethod.POST)
+    public String signIn(String returnUrl) {
+        return "login/login";
+    }
+
+    @RequestMapping(value = "/login" ,method = RequestMethod.GET)
+    public String login() {
+        return "login/login";
+    }
+    @RequestMapping("/hxy")
+    public String GrantVC(){
+        System.out.println("jkjfdlskfjlkdsajfkladsklf");
+        return "VA";
+    }
+}
